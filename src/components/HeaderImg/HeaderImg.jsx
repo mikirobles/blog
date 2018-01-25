6eab851e19fbbcb87a9e16f5d335a7af64193263
Micael Robles <micael.robles@gmail.com>
2018-01-24 21:17:12 -0300

    load all images from https

--- src/components/HeaderImg/HeaderImg.jsx
+++ src/components/HeaderImg/HeaderImg.jsx
@@ -5,7 +5,7 @@ import fetch from 'isomorphic-fetch'
 
 const getRandomImage = async () => {
   const json = await (await fetch('https://archillect-api.now.sh/random')).json()
-  return json.source;
+  return json.source.replace('http:', 'https:');
 }
 
 const HeaderImgContainer = styled.div`
